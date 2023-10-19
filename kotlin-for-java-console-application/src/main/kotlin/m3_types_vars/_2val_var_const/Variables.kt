7d710ad293b5cd14c94f2be01852b4d24f44f713
J. Junior <junior.jaava@gmail.com>
2023-10-19 18:56:55 +0200

    var? val? const?
    Signed-off-by: J. Junior <junior.jaava@gmail.com>

---
+++ kotlin-for-java-console-application/src/main/kotlin/m3_types_vars/_2val_var_const/Variables.kt
@@ -0,0 +1,15 @@
+package for_java_devs.m3_types_vars._2val_var_const
+
+fun main() {
+
+    var mutable = 1
+    mutable = 2
+
+    val immutable = 3
+//    immutable = 4
+
+}
+
+val staticLikeTopLevelVar = "thing"
+
+const val UNIX_LINE_SEPARATOR = "\n"
