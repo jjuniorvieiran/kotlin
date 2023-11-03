46f5421aa7e3702ce04ee824554cdea77315c6bb
J. Junior <junior.jaava@gmail.com>
2023-11-03 18:02:46 +0100

    kotlin OOP basics
    Signed-off-by: J. Junior <junior.jaava@gmail.com>

---
+++ kotlin-for-java-console-application/src/main/kotlin/m6_oop/_1classes_interfaces/TranslationServiceDemo.kt
@@ -0,0 +1,31 @@
+package for_java_devs.m6_oop._1classes_interfaces.kotlin
+
+interface TranslationService {
+
+    fun translate(sourceLanguage: String, targetLanguage: String, input: String): String
+
+    fun getSupportedLanguages(): List<String> {
+        return listOf("English", "Spanish", "French")
+    }
+}
+
+abstract class AbstractTranslationService(private val apiKey: String) : TranslationService {
+    fun getApiKey(): String {
+        return apiKey
+    }
+}
+
+
+class GoogleTranslationService(apiKey: String) : AbstractTranslationService(apiKey) {
+    override fun translate(sourceLanguage: String, targetLanguage: String, input: String): String {
+        // Use Google Cloud Translation API
+        // https://cloud.google.com/translate
+        return ""
+    }
+}
+
+//class SneakySubclass : GoogleTranslationService
+
+fun main() {
+    val translator = GoogleTranslationService("abc-123")
+}
